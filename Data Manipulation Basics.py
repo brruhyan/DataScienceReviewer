62ef9fba1862db20932b306312c3e6114840f9fd
brruhyan <134396548+brruhyan@users.noreply.github.com>
2024-07-21 11:29:15 +0800

    Update Data Manipulation Basics.py

--- Data Manipulation Basics.py
+++ Data Manipulation Basics.py
@@ -7,3 +7,12 @@ df = df.sort_values('column_name')
 #Sorting two columns at once, sorting column_name1 by ascending order and column_name2 by descending order
 df = df.sort_values('column_name', ascending = False) 
 df = df.sort_values(['column_name1, column_name2'], ascending = [True, False]) 
+
+#Selecting a specific column in a dataframe
+new_df = df[['column_name1', 'column_name2']]
+#Subsetting based on metrics
+new_df = df[df['column_name'] > 1000]
+new_df = df[(df["column_name1"] < 1000) & (df["column_name2"] == "metric")]
+
+#Creating new columns in a dataframe based on an operation
+new_df['new_column'] = df['column_name1'] + df['column_name2']
