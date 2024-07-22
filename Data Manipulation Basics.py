4a6e66cd7f67da18254c2beefe2685f830a96400
brruhyan <134396548+brruhyan@users.noreply.github.com>
2024-07-22 11:01:49 +0800

    Update Data Manipulation Basics.py

--- Data Manipulation Basics.py
+++ Data Manipulation Basics.py
@@ -16,3 +16,16 @@ new_df = df[(df["column_name1"] < 1000) & (df["column_name2"] == "metric")]
 
 #Creating new columns in a dataframe based on an operation
 new_df['new_column'] = df['column_name1'] + df['column_name2']
+
+#join data using pandas (inner join), the inner join returns values with those rows that have common characteristics. 
+joined_df = dataframe1.merge(dataframe2, on = 'common_column', #this only works when both dataframes have common columns
+                      suffixes = ('_df1' , 'df2'))       #the suffixes is there to determine which table the values came from
+
+#three tables merging
+joined_df = dataframe1.merge(dataframe2, on = 'common_column').merge(dataframe3, on = 'common_column') 
+joined_df = dataframe1.merge(dataframe2, on = 'common_column').merge(dataframe3, on = 'common_column',
+                                                                    suffixes = ('_df1', 'df2'))
+
+#join using pandas (left join), left join returns all values of the left dataframe (dataframe1)
+#all common columns found in the right (dataframe2) dataframe will be added to the left dataframe
+joined_df = dataframe1.merge(dataframe2, on = 'common_column', how = 'left') 
