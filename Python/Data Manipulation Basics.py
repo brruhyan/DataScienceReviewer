adf616311836ea9d3e39e598fd3f37c3f23d87ce
brruhyan <134396548+brruhyan@users.noreply.github.com>
2024-07-26 20:53:05 +0800

    Update Data Manipulation Basics.py

--- Python/Data Manipulation Basics.py
+++ Python/Data Manipulation Basics.py
@@ -1,7 +1,3 @@
-#This is a data analyst reviewer solely created for my review for the data scientist certification.
-#This reviewer could also serve as a steppping stone for aspiring data analyst such as myself. 
-#Feel free to share this with your peers!"""
-
 #Sorting values by a specific column
 df = df.sort_values('column_name')
 #Sorting two columns at once, sorting column_name1 by ascending order and column_name2 by descending order
