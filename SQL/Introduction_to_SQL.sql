8d8ddd7cd55c544123ac5574ede61dd13cd2e162
brruhyan <134396548+brruhyan@users.noreply.github.com>
2024-07-26 13:37:04 +0800

    Update Introduction_to_SQL.sql

--- SQL/Introduction_to_SQL.sql
+++ SQL/Introduction_to_SQL.sql
@@ -18,3 +18,43 @@ FROM graduation_data;
 CREATE VIEW graduation_student_data AS
 SELECT student_id, name, year_graduated
 FROM graduation_data;
+
+-- counting number of records (similar to value_counts in Python)
+SELECT COUNT(graduation_date) AS count_graduation_date
+FROM graduation_data;
+SELECT COUNT(*) AS total_records
+from graduation_data;
+
+-- counting unique values in a database
+SELECT COUNT(DISTINCT graduation_date) AS distinct_graduation
+FROM graduation_data;
+
+-- Filtering
+SELECT student_name
+FROM graduation_data WHERE graduation_date => 2010;
+
+-- Filtering with multiple criterias
+SELECT * FROM graduaton_data
+WHERE school = 'TIP_QC' OR school = 'TIP_MNL';
+WHERE school = 'TIP_QC' AND graduation_date = 2010;
+WHERE graduation_date BETWEEN 2010 AND 2015
+
+-- multiple filtering conditions (selects students who graduation in 2010 and 2015 with the specific course)
+WHERE (graduation_date = 2010 OR graduation_date = 2015)
+AND (student_course = 'CpE' OR student_course = 'CE');
+
+-- multiple filtering conditions (specified range)
+WHERE graduation_date BETWEEN 2010 AND 2015
+
+-- multiple filtering conditions 
+WHERE graduation_data BETWEEN 2010 AND 2015 AND student_course = 'CpE';
+
+-- Filtering text (Will select names starting with Ch)
+SELECT name FROM graduation_data
+WHERE name LIKE 'Ch%'; 
+
+-- Filtering text (will only match three letter names starting from Ev)
+WHERE name LIKE 'Ev_' ;
+
+-- Filtering text (will not select names starting with A)
+WHERE name NOT LIKE 'A%';
