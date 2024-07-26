dbf5ef26a79e586fb3c70a4ac5dc589d07e654ab
brruhyan <134396548+brruhyan@users.noreply.github.com>
2024-07-26 20:09:24 +0800

    Update Aggregation_Functions_SQL.sql

--- SQL/Aggregation_Functions_SQL.sql
+++ SQL/Aggregation_Functions_SQL.sql
@@ -1,1 +1,8 @@
-  -- Summarizing data
+-- Summarizing data Different functions include SUM(), MIN(), MAX(), and COUNT()
+SELECT AVG(student_grade)
+FROM graduation_data
+
+-- Summarizing data with the WHERE and ROUND parameter, the number denotes the decimal places
+SELECT ROUND(AVG(student_grade), 2) AS average_student_grade
+FROM graduation_data 
+WHERE graduation_date >= 2015;
