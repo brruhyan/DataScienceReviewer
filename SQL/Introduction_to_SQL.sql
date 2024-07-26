9724669d46c5294dccf2ce2311b808237a1cd7ae
brruhyan <134396548+brruhyan@users.noreply.github.com>
2024-07-26 12:49:12 +0800

    Update Introduction_to_SQL.sql

--- SQL/Introduction_to_SQL.sql
+++ SQL/Introduction_to_SQL.sql
@@ -6,3 +6,15 @@ FROM table_name -- which table to select the columns from
 SELECT name, student_number -- replace columns with * to select all immediately
 FROM graduation_data; -- make sure to end the query with a semicolon
 
+-- Aliasing
+SELECT name AS first_name, year_graduated
+FROM graduation_data;
+
+-- Selecting values with no repeat values
+SELECT DISTINCT year_graduated
+FROM graduation_data;
+
+-- Creating views (simular to subsets in python)
+CREATE VIEW graduation_student_data AS
+SELECT student_id, name, year_graduated
+FROM graduation_data;
