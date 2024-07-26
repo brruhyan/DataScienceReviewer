ab15aa28cdb3632bca76948dd1a59313a31de727
brruhyan <134396548+brruhyan@users.noreply.github.com>
2024-07-26 12:39:48 +0800

    Create Introduction_to_SQL.sql

---
+++ SQL/Introduction_to_SQL.sql
@@ -0,0 +1,8 @@
+-- Notable keywords to remember
+SELECT column_name -- Indicates which fields to be selected
+FROM table_name -- which table to select the columns from
+
+-- Sample query code
+SELECT name, student_number -- replace columns with * to select all immediately
+FROM graduation_data; -- make sure to end the query with a semicolon
+
