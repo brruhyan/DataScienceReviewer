c332ed4e4edbbb5b2289fb40151c21b064997963
brruhyan <134396548+brruhyan@users.noreply.github.com>
2024-07-27 18:56:08 +0800

    Update Data_Manipulation_in_SQL.sql

--- SQL/Data_Manipulation_in_SQL.sql
+++ SQL/Data_Manipulation_in_SQL.sql
@@ -2,7 +2,16 @@
 -- records that dont have a match will be discarded/omitted
 
 SELECT prime_ministers.country, prime_ministers.continent, prime_minister, president -- select the fields you want included in the final table
-FROM prime_ministers -- left table
-INNER JOIN presidents -- right table (table to join the values to the left)
-ON prime_ministers.country = president.country; -- values in each table to match (make sure they have common values)
+FROM prime_ministers AS left_table-- left table
+INNER JOIN presidents AS right_table -- right table (table to join the values to the left)
+ON left_table.country = right_table.country; -- values in each table to match (make sure they have common values)
 
+-- sample query application (inner join)
+SELECT cities.name AS city, countries.name AS country, region
+FROM cities
+INNER JOIN countries ON cities.country_code = countries.code; 
+
+-- sample query (inner join 2)
+SELECT c.code AS country_code, name, year, inflation_rate
+FROM countries AS c
+INNER JOIN economies as e ON c.code = e.code;
