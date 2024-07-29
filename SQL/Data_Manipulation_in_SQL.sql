ae5fe3fba69234bfa06195811ed61b3fa51a43f7
brruhyan <134396548+brruhyan@users.noreply.github.com>
2024-07-29 12:39:43 +0800

    Update Data_Manipulation_in_SQL.sql

--- SQL/Data_Manipulation_in_SQL.sql
+++ SQL/Data_Manipulation_in_SQL.sql
@@ -15,3 +15,37 @@ INNER JOIN countries ON cities.country_code = countries.code;
 SELECT c.code AS country_code, name, year, inflation_rate
 FROM countries AS c
 INNER JOIN economies as e ON c.code = e.code;
+
+-- sample query (step by step)
+FROM countries AS c -- select the left table
+INNER JOIN languages AS l -- select the right table
+SELECT c.name AS country, l.name AS language, official -- select which to show in the merged table
+USING(name); -- select which common column to merge with
+
+-- Multiple joins
+-- Joins on joins
+FROM left_table -- selecting the main table
+INNER JOIN right_table -- selecting the right table
+SELECT * ON left_table.id = right_table.id -- selecting all columns and then performing join on id
+INNER JOIN another_table -- creating another table 
+ON left_table_id = another_table.id -- joining another table to the results of the initial join
+
+-- sample query (multiple join)
+FROM prime_minister AS p1
+INNER JOIN presidents as p2
+SELECT p1.country, p1.continent, president, prime_minister
+USING(country)
+INNER JOIN prime_mister_terms AS p3
+USING(prime_minister);
+
+-- sample query 2 (multiple join)
+FROM left_table INNER join right_table
+SELECT * ON left_table.id = right_table.id
+AND left_table.date = right_table.date; 
+
+-- left join 
+-- all values are merged but those without matching will be specified as NULL;
+FROM prime_minister AS p1
+LEFT JOIN presidents as p2
+SELECT p1.country, prime_minister, president 
+USING(country);
