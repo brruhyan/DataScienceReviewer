c58aefaee6021bdade3d5126338233669a4d6d77
brruhyan <134396548+brruhyan@users.noreply.github.com>
2024-07-31 11:42:45 +0800

    Update Machine_Learning_scikit-learn (Supervised).py

--- Python (Deep Learning)/Machine_Learning_scikit-learn (Supervised).py
+++ Python (Deep Learning)/Machine_Learning_scikit-learn (Supervised).py
@@ -1,1 +1,8 @@
+# Classification models
 
+# k-Nearest Neighbors (first step)
+from sklearn.neighbors import KNeighborsClassifier
+x = df[['total_day_income', 'total_night_income']].values
+y = df['churn'].values
+# second step
+knn = KNeighborsClassifier(n_neighbords = 15)
