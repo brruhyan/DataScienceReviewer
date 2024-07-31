56e0da2ad7fa03e7a8a7ebfa6ee84d390558fe22
brruhyan <134396548+brruhyan@users.noreply.github.com>
2024-07-31 11:31:06 +0800

    Update Set_Theories_SQL_Joins.sql

--- SQL/Set_Theories_SQL_Joins.sql
+++ SQL/Set_Theories_SQL_Joins.sql
@@ -45,9 +45,34 @@ SELECT DISTINCT name FROM languages -- selecting only distinct names in the lang
       WHERE region = 'Middle East');
 
 -- anti joins
+-- same as semi join but it includes records that are not equal to the condition
 SELECT country, president FROM presidents
 WHERE continent LIKE '%America'  
   AND country NOT IN
     (SELECT country FROM states
      WHERE indep_year < 1800);
 
+-- subquery within the WHERE clause
+WHERE DISTINCT continent, -- makes sure that no repeat continents are included
+  (SELECT COUNT(*) FROM monarchs  -- will count the number of monarchs within each continent
+      WHERE states.continent = monarch.continent) AS monarch_count --merges both into one column and outputs if there is a monarch.
+FROM states;
+
+-- sample query (subquery within WHERE)
+SELECT * FROM populations -- selects all values in the table
+  WHERE life_expectancy > 1.15 * -- gets the records where life_expectancy is times 1.15 than the average
+    (SELECT AVG(life_expectancy) FROM populations
+      WHERE year = 2015) AND year = 2015;
+
+-- sample query 2 
+SELECT name, country_code, urbanarea_pop FROM cities
+  WHERE name IN 
+    (SELECT capital FROM countries) 
+ORDER BY urbanarea_pop DESC;
+
+-- Subqueries within FROM
+SELECT DISTINCT monarchs.continent, sub.most_recent
+FROM monarchs,
+  (SELECT continent, MAX(indep_year) AS most_recent
+    FROM states GROUP BY continent) AS sub
+WHERE monarchs.continent = sub.continent
