d2f6a215cc699e238b72707db20aeb2c211d44f4
brruhyan <134396548+brruhyan@users.noreply.github.com>
2024-07-31 11:56:40 +0800

    Update Machine_Learning_scikit-learn (Supervised).py

--- Python (Deep Learning)/Machine_Learning_scikit-learn (Supervised).py
+++ Python (Deep Learning)/Machine_Learning_scikit-learn (Supervised).py
@@ -18,3 +18,24 @@ X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.3,
 knn = KNeighborsClassifier(n_neighbords = 15)
 knn.fit(x,y)
 print(knn.score(X_test, y_test))
+
+#model complexity curve 
+train_accuracies = {}
+test_accuracies = {}
+neighbors = np.arange(1,26)
+#looping through the array
+for neighbor in neighbors:
+  knn = KNeighborsClassifier(n_neighbors = neighbor)
+  knn.fit(X_train, y_train)
+  train_accuracies[neighbor] = knn.score(X_train, y_train)
+  test_accuracies[neighbor] = knn.score(X_test, y_test)
+#plotting the results
+plt.figure(figsize = (8,6))
+plt.title('KNN Complexity')
+plt.plot(neighbors, train_accuracies.values(), label = 'Training Accuracy')
+plt.plot(neighbors, test_accuracies.values(), label = 'Testing Accuracy')
+plt.legend()
+plt.xlabel("Varying Number of Neighbors")
+plt.ylabel("Accuracy")
+plt.show()
+        
