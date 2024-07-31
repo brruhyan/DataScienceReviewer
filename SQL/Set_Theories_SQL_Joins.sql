18dfa39025a4a1b512814e03abff8d58483be1c4
brruhyan <134396548+brruhyan@users.noreply.github.com>
2024-07-31 10:57:34 +0800

    Update Set_Theories_SQL_Joins.sql

--- SQL/Set_Theories_SQL_Joins.sql
+++ SQL/Set_Theories_SQL_Joins.sql
@@ -26,3 +26,28 @@ FROM monarchs
   EXCEPT
 SELECT prime_minister, country 
 FROM prime_ministers;
+
+-- Additive joins
+-- Semi joining (chooses records in the first table where a condition is met in the second table)
+SELECT country FROM states
+WHERE indep_year < 1800;
+
+-- sample query (semi join)
+SELECT president, country, continent FROM presidents
+WHERE country IN
+    (SELECT country FROM states 
+      WHERE indep_year < 1800);
+
+--sample query 2 (semi join, basically what it does it merges two subtables into one)
+SELECT DISTINCT name FROM languages -- selecting only distinct names in the languages table
+  WHERE code IN -- selecting only the code where the country is equal to middle east
+    (SELECT code FROM countries 
+      WHERE region = 'Middle East');
+
+-- anti joins
+SELECT country, president FROM presidents
+WHERE continent LIKE '%America'  
+  AND country NOT IN
+    (SELECT country FROM states
+     WHERE indep_year < 1800);
+
