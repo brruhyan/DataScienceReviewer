0cc2489dcc48cf0b726ca79fc41db6473d8e1a2e
brruhyan <134396548+brruhyan@users.noreply.github.com>
2024-08-01 14:30:24 +0800

    Update Clustering_Data (Unsupervised).py

--- Python (Deep Learning)/Clustering_Data (Unsupervised).py
+++ Python (Deep Learning)/Clustering_Data (Unsupervised).py
@@ -1,1 +1,10 @@
 # Clustering data
+
+# -----------------------
+
+# k-means clustering
+from sklearn.cluster import KMeans
+model = KMeans(n_clusters = 3)
+model.fit(samples)
+labels = model.predict(samples)
+print(labels)
