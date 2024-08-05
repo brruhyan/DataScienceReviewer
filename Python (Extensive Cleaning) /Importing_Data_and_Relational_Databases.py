3bd8048b3a22f9e2c780147fb0c719108e7d341a
brruhyan <134396548+brruhyan@users.noreply.github.com>
2024-08-05 17:57:08 +0800

    Create Importing_Data_and_Relational_Databases.py

---
+++ Python (Extensive Cleaning) /Importing_Data_and_Relational_Databases.py
@@ -0,0 +1,45 @@
+# ----- importing basics -----
+
+# importing text files
+with open('filename_smth.txt', 'r') as file:
+  print(file.read())
+
+# importing flat files using numpy (arrays)
+filename = 'filename_here.txt'
+data = np.loadtxt(filename, delimiter = ',') #skiprows = 1, #usecols = [0,2]
+
+# importing flat files using pandas
+df = pd.read_csv('filename.csv')
+data_array = df.to_numpy #(converting to numpy array)
+
+# importing with pandas with custom params
+df = pd.read_csv('filename.csv', sep = '\t', comment = '#', na_values = ['NaN'])
+
+# ----- importing different file types (EXCEL) -----
+
+# pickled files
+import pickle 
+with open('pickled_file.pkl', 'rb') as file: #read-only binary
+  data = pickle.load(file)
+print(data)
+
+# excel sheets
+file = 'excel_filename.xlsx'
+data = pd.ExcelFile(file)
+print(data.sheet_names)
+
+# parcing a sheet into a dataframe
+df = xls.parse('sheet_filename')
+
+# ----- importing different file types (SAS/STATA) -----
+
+# importing SAS files
+import pandas as pd
+from sas7bdat import SAS7BDAT
+with SAS7BDAT('filename_here.sas7bdat') as file:
+  df_sas = file.to_data_frame()
+
+# importing STATA files
+data = pd.read_stata('filename_here.dta')
+
+# importing HDF5 files 
