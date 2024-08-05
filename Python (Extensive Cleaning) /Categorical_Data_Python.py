aece72d4dc5903f5a9b4783c24cc7826350b65c6
brruhyan <134396548+brruhyan@users.noreply.github.com>
2024-08-05 12:08:50 +0800

    Update Categorical_Data_Python.py

--- Python (Extensive Cleaning) /Categorical_Data_Python.py
+++ Python (Extensive Cleaning) /Categorical_Data_Python.py
@@ -1,1 +1,26 @@
+# initial exploration of categorical data
 
+print(student_data['average_grade'].describe())
+print(student_data['average_grade'].value_counts()) #frequency table
+print(student_data['average_grade'].value_counts(normalize = True)) #relative frequency
+
+# setting an object dtype to categorical (categorical saves data)
+
+student_data['Course'] = student_data['Course'].astype('category')
+
+# creating a categorical series
+
+my_categories = ['CpE', 'CE', 'ME' 'ENSE']
+new_category = pd.Series(my_categories, dtype = 'category')
+
+# creating a categorical series with order
+my_categories = ['CpE', 'CE', 'ME' 'ENSE']
+new_category = pd.Categorical(my_categories, categories = ['ENSE', 'CpE', 'CE', 'ME'], ordered = True)
+
+# specifiying dtypes when reading data
+student_dtypes = {'Course' : 'category'}
+student = pd.read_csv('student_data.csv', dtype = student_dtypes)
+
+# ------------------------------------
+
+#grouping data by categories in pandas
