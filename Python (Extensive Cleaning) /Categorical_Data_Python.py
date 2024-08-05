cbe6c436af8a3abb51cc2694ac329ec55fa04794
brruhyan <134396548+brruhyan@users.noreply.github.com>
2024-08-05 12:32:51 +0800

    Update Categorical_Data_Python.py

--- Python (Extensive Cleaning) /Categorical_Data_Python.py
+++ Python (Extensive Cleaning) /Categorical_Data_Python.py
@@ -5,11 +5,9 @@ print(student_data['average_grade'].value_counts()) #frequency table
 print(student_data['average_grade'].value_counts(normalize = True)) #relative frequency
 
 # setting an object dtype to categorical (categorical saves data)
-
 student_data['Course'] = student_data['Course'].astype('category')
 
 # creating a categorical series
-
 my_categories = ['CpE', 'CE', 'ME' 'ENSE']
 new_category = pd.Series(my_categories, dtype = 'category')
 
@@ -23,4 +21,29 @@ student = pd.read_csv('student_data.csv', dtype = student_dtypes)
 
 # ------------------------------------
 
-#grouping data by categories in pandas
+# grouping data by categories in pandas
+student_group = student_data.groupby(by = ['Course'].mean())
+
+# grouping data and specifiying the columns to aggregate
+student_group = student_data.groupby(by = ['Course'])['Age', 'ave_grade'].sum()
+
+# grouping by multiple columns 
+student_group = student_data.groupby(by = ['Course', 'Sex'])['average_grade].mean()
+
+# ------------------------------------
+
+# settings category values
+student['course'] = student['course'].astype('category')
+student['course'].value_counts(dropna = False)
+
+# overwriting categories 
+student['course'] = student['course'].cat.set_categories(
+  new_categories = ['CpE', 'IE', 'ME' 'ENSE', 'IT'], ordered = True)
+
+# adding new categories 
+student['course'] = student['course'].cat.add_categories(
+  new_categories = ['CS', 'IS'])
+
+# removing categories
+student['course'] = student['course'].cat.remove_categories(
+  removals = ['CS', 'IS'])
